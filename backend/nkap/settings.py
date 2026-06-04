820c3d7fd6fab52ba3f879ec61d3990f74689488
c4zxs56vtw-hub <onbionoelogo69@icloud.com>
2026-06-04 17:32:36 +0100

    perf(auth): use MD5 hasher in debug mode to speed up auth operations

--- backend/nkap/settings.py
+++ backend/nkap/settings.py
@@ -88,6 +88,12 @@ else:
     }
 
 
+if DEBUG:
+    PASSWORD_HASHERS = [
+        "django.contrib.auth.hashers.MD5PasswordHasher",
+    ]
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
